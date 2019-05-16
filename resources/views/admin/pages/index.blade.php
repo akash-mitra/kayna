310b34d913d09dbbd4799745f84129b530da4b6c
Akash Mitra <akashmitra@gmail.com>
2019-05-16 20:39:56 +0800

    Fixed #7

--- resources/views/admin/pages/index.blade.php
+++ resources/views/admin/pages/index.blade.php
@@ -32,64 +32,43 @@
 </div>
 
 <div class="w-full px-6">
-    <table class="w-full bg-white shadow rounded text-left table-collapse">
-        <thead class="uppercase text-xs font-semibold text-grey-darker border-b-2">
-            <tr>
-                <th class="p-4 hidden sm:table-cell">Actions</th>
-                <th class="p-4">Page</th>
-                <th class="p-4 hidden sm:table-cell">Stats</th>
-            </tr>
-        </thead>
-        <tbody class="align-baseline">
-            <tr v-for="page in filter_pages" :class="page.status != 'Live' ? 'bg-grey-lightest': 'shadow-inner'" class="flex sm:table-row flex-col-reverse sm:flex-row">
-                <td class="px-4 py-2 font-mono text-xs whitespace-no-wrap flex sm:flex-col items-end border-b-2 sm:border-none bg-grey-lightest sm:bg-transparent">
-                    <a v-bind:href="page.url" target="_blank" class="p-2 cursor-pointer text-indigo-dark no-underline hover:text-blue">View</a>
+    
+    <div v-for="page in filter_pages" :class="page.status != 'Live' ? 'bg-grey-lighter': 'bg-white'" class="my-6 rounded">
 
-                    <button @click="changeStatus(page.id, (page.status != 'Live'? 'Live' : 'Draft'))" v-text="page.status != 'Live'? 'Publish' : 'Take Down'" class="p-2 cursor-pointer text-indigo-dark no-underline hover:text-blue"></button>
-
-                    <button @click="deletePage(page.id)" class="p-2 cursor-pointer text-indigo-dark no-underline hover:text-blue">Delete</button>
-                </td>
-                <td class="py-2 px-4 max-w-sm text-sm">
-
-                    <p v-text="page.category.name + ' / '" class="text-grey text-sm mb-2"></p>
-
-                    <div class="bg-grey-lightest p-4 rounded-lg">
-                        <a v-bind:href="editPage(page.id)" class="no-underline text-blue text-sm font-medium">
-                            <span v-text="page.title"></span>
+        <div class="w-full sm:flex px-6 py-2">
+            
+                <div class="w-full sm:w-3/4">
+                        <a v-bind:href="editPage(page.id)" class="no-underline text-blue text-sm block pt-3">
+                            <span v-text="page.title" class="text-sm font-semibold"></span>
                         </a>
-                        <p class="py-2 text-sm" v-text="page.summary"></p>
-                    </div>
-                    <div class="my-2 flex text-grey-dark italic justify-between items-center">
-                        <div class="flex items-center">
+                        <p v-text="page.summary" class="pt-4 text-sm text-grey-darkest"></p>
+                        <p class="py-2 flex items-center text-xs text-grey-dark">
                             <a :href="page.author.url" class="no-underline">
                                 <img v-if="page.author.avatar" v-bind:src="page.author.avatar" class="w-6 h-6 rounded-full mr-4">
-                                <svg v-if="!page.author.avatar" class="w-6 h-6 rounded-full border mr-4 fill-current text-grey-lighter" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                                    <path class="heroicon-ui" d="M12 12a5 5 0 1 1 0-10 5 5 0 0 1 0 10zm0-2a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm9 11a1 1 0 0 1-2 0v-2a3 3 0 0 0-3-3H8a3 3 0 0 0-3 3v2a1 1 0 0 1-2 0v-2a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v2z" /></svg>
+                                <svg v-if="!page.author.avatar" class="w-6 h-6 rounded-full border mr-2 fill-current text-grey-lighter" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                                    <path class="heroicon-ui" d="M12 12a5 5 0 1 1 0-10 5 5 0 0 1 0 10zm0-2a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm9 11a1 1 0 0 1-2 0v-2a3 3 0 0 0-3-3H8a3 3 0 0 0-3 3v2a1 1 0 0 1-2 0v-2a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v2z" />
+                                </svg>
                             </a>
+                            <span>Posted under @{{page.category.name}}, updated @{{page.ago}}</span>
+                        </p>
 
-                            <a v-bind:href="page.author.url" class="no-underline text-blue-light">
-                                <span class="" v-text="page.author.name"></span>
-                            </a>
+                        <div class="w-full my-2 py-2 text-xs text-orange-dark">
+                            <span class="p-1 border rounded border-red-lighter" v-if="page.metadesc === null">No Meta Description</span>
+                            <span class="p-1 border rounded border-red-lighter" v-if="page.metakey === null">No Meta Key</span>
                         </div>
-                        <div class="hidden sm:table-cell">
-                            <svg viewBox="0 0 24 24" class="heroicon h-5 w-5 fill-current text-grey" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                                <path d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm1-8.41l2.54 2.53a1 1 0 0 1-1.42 1.42L11.3 12.7A1 1 0 0 1 11 12V8a1 1 0 0 1 2 0v3.59z"></path>
-                            </svg>
+                </div>    
+                <div class="w-full sm:w-1/4 py-2">
+                    
+                </div>
+        </div>
 
-                            <span class="" v-text="page.ago"></span>.
-                        </div>
-                    </div>
-                </td>
-                <td class="p-4 text-sm hidden sm:table-cell">
-                    <div class="italic text-grey-dark">
-                        <span class="font-medium text-blue font-mono" v-text="page.comments.length"></span> Comments
-                    </div>
-                </td>
-
-            </tr>
-        </tbody>
-    </table>
-
+        <div class="px-6 bg-grey-lightest text-sm py-1 border-b">
+                <a v-bind:href="page.url" target="_blank" class="p-2 cursor-pointer text-indigo-darker no-underline hover:text-blue">View</a>
+                <button @click="changeStatus(page.id, (page.status != 'Live'? 'Live' : 'Draft'))" v-text="page.status != 'Live'? 'Publish' : 'Take Down'" class="p-2 cursor-pointer text-indigo-darker no-underline hover:text-blue"></button>
+                <button @click="deletePage(page.id)" class="p-2 cursor-pointer text-indigo-darker no-underline hover:text-blue">Delete</button>
+        </div>
+        
+    </div> <!-- v-if ends -->
 </div>
 
 <div class="px-6 text-xs text-right py-4 text-grey-darker">
@@ -109,12 +88,13 @@
         },
 
         computed: {
-
             filter_pages: function() {
-                return this.pages.filter(page =>
-                    page.title.indexOf(this.needle) != -1 ||
-                    page.author.name.indexOf(this.needle) != -1
-                )
+                return this.pages.filter(page => {
+                    let t = page.title.toLowerCase()
+                    let a = page.author.name.toLowerCase()
+                    return t.indexOf(this.needle.toLowerCase()) != -1 
+                            || a.indexOf(this.needle.toLowerCase()) != -1
+                })
             }
 
         },
